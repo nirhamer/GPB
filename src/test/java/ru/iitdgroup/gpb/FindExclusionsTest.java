4e4a21e96bf3c0fc27e4b45ab9454e8806e7a4bf
nir <nirhamer17@walla.co.il>
2021-06-18 16:25:51 +0300

    fixed java incompatible name

---
+++ src/test/java/ru/iitdgroup/gpb/FindExclusionsTest.java
@@ -0,0 +1,13 @@
+package ru.iitdgroup.gpb;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindExclusionsTest {
+
+    @Test
+    void main() {
+
+    }
+}
