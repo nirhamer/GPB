3ca1a20c3103bdd4e367e2be2a1243a1357550b3
nir <nirhamer17@walla.co.il>
2021-07-26 19:14:29 +0300

    .

--- src/main/java/ru/iitdgroup/gpb/ScanFileSystem.java
+++ src/main/java/ru/iitdgroup/gpb/ScanFileSystem.java
@@ -7,15 +7,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ScanFileSystem {
 
     static Set<String> exclusionsSet = new HashSet<>();
     private static final String AS_ROOT = ".";
 
-    public static void main(String[] args) {
+    public static void getExcludedFiles(String[] args) {
 
         //region reading the exclusions file
         File myObj = new File("exclusions.txt");
@@ -35,11 +38,13 @@ public class ScanFileSystem {
     }
 
 
-    public void walk(String path) throws IOException {
-
-        Files.walk(Path.of(AS_ROOT));
+    public static void main(String[] args) throws IOException {
 
 
+        Stream<Path> s = Files.walk(Path.of(AS_ROOT));
+
+        List<Path> myList = s.collect(Collectors.toList());
 
     }
+
 }
