3a67321e934205067772f913b4b53cd68d5e0bf3
nir <nirhamer17@walla.co.il>
2021-08-09 18:41:53 +0300

    .

--- src/main/java/ru/iitdgroup/gpb/ScanFileSystem.java
+++ src/main/java/ru/iitdgroup/gpb/ScanFileSystem.java
@@ -4,16 +4,21 @@ package ru.iitdgroup.gpb;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static ru.iitdgroup.gpb.App.toHexString;
 
 public class ScanFileSystem {
 
@@ -39,14 +44,41 @@ public class ScanFileSystem {
         //endregion
     }
 
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
 
-    public static void main(String[] args) throws IOException {
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
 
 
-        Stream<Path> st = Files.walk(Path.of(AS_ROOT))
-                .filter(s -> Files.isRegularFile(s, LinkOption.NOFOLLOW_LINKS))
-                .filter(UseCase1::checkPath);
+    public void walk( String path ) {
+        File root = new File( path );
+        File[] list = root.listFiles();
 
-        List<Path> myList = st.collect(Collectors.toList());
+
+        if (list == null) return;
+
+        for ( File f : list ) {
+            System.out.println("\n" + f.getName() + " : "  );
+            if (exclusionsSet.contains(f.getName())) { continue; }
+            if ( f.isDirectory() ) {
+                walk( f.getAbsolutePath() );
+                System.out.println( "Dir:" + f.getAbsoluteFile() );
+            }
+            else {
+                new StringBuilder();
+                try {
+                    Scanner myReader = new Scanner(f);
+                    System.out.println("\n" + f.getName() + " : " + toHexString(getSHA("")));
+                    myReader.close();
+                } catch (FileNotFoundException | NoSuchAlgorithmException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+                System.out.println( "File:" + f.getAbsoluteFile() );
+            }
+        }
+        App fw = new App();
+        fw.walk("." );
     }
 }
